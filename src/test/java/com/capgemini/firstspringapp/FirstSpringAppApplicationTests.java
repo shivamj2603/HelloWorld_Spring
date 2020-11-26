8667a4e70de9ad3b241d4fe3e35fead50ee6c95b
shivamj2603 <shivamj2603@gmail.com>
2020-11-26 13:11:31 +0530

    Implementation of Hello World using Spring Rest

---
+++ src/test/java/com/capgemini/firstspringapp/FirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
